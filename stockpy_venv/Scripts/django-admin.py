ef85f9d6e10e644524724f6d09ecf397ea0f1e42
CyborgVillager <almawijonathan@gmail.com>
2019-12-11 10:06:31 -0600

    v0

---
+++ stockpy_venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\PyProject\GitRespo\stock_pydjango\ja_stock_pydjango\stockpy_venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
