c68df7673065957015667d5a9b52dbe527e621a3
CyborgVillager <almawijonathan@gmail.com>
2019-12-12 14:59:41 -0600

    v12

--- stocks/quotes/views.py
+++ stocks/quotes/views.py
@@ -8,15 +8,19 @@ def home(request):
     # getting information for the APi
     import requests
     import json
-    api_request = requests.get('https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_d5d97aef30904e3582a9badd3d0478b3')
 
-    try:
-        # getting the api data, after that parse the info, else show error
-        api = json.loads(api_request.content)
-    except Exception as error:
-        api = 'Error, try again!'
-    # pk_d5d97aef30904e3582a9badd3d0478b3
-    return render(request, 'home.html', {'api': api})
+    if request.method == 'POST':
+        ticker = request.POST['ticker']
+        api_request = requests.get("https://cloud.iexapis.com/stable/stock/" + ticker + "/quote?token=pk_d5d97aef30904e3582a9badd3d0478b3")
+
+        try:
+            # getting the api data, after that parse the info, else show error
+            api = json.loads(api_request.content)
+        except Exception as error:
+            api = 'Error, try again!'
+        return render(request, 'home.html', {'api': api})
+    else:
+        return render(request, 'home.html', {'ticker': 'Please Enter a Ticker Symbol'})
 
 
 def about(request):
