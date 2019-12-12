0ce103c6293425662bb484d6ed1538002865ea52
CyborgVillager <almawijonathan@gmail.com>
2019-12-12 11:19:43 -0600

    v9
    install requests, aquired api & api info was a success, data is rough at the moment

--- stocks/quotes/views.py
+++ stocks/quotes/views.py
@@ -5,7 +5,18 @@ from django.shortcuts import render
 
 # browser request for home.html , about, contact
 def home(request):
-    return render(request, 'home.html', {})
+    # getting information for the APi
+    import requests
+    import json
+    api_request = requests.get('https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_d5d97aef30904e3582a9badd3d0478b3')
+
+    try:
+        # getting the api data, after that parse the info, else show error
+        api = json.loads(api_request.content)
+    except Exception as error:
+        api = 'Error, try again!'
+    # pk_d5d97aef30904e3582a9badd3d0478b3
+    return render(request, 'home.html', {'api': api})
 
 
 def about(request):
