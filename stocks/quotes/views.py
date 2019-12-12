c02136e37f7a61ee99a477aadf079e689e796669
CyborgVillager <almawijonathan@gmail.com>
2019-12-12 09:25:40 -0600

    ver6
    added about & contact .html pages
    will improve in later ver#

--- stocks/quotes/views.py
+++ stocks/quotes/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 
-# browser request for home.html
+# browser request for home.html , about, contact
 def home(request):
     return render(request, 'home.html', {})
+
+
+def about(request):
+    return render(request, 'about.html', {})
+
+
+def contact(request):
+    return render(request, 'contact.html', {})
