5b8bc3582d5b3b268574b78a34335e8d0e05233c
CyborgVillager <almawijonathan@gmail.com>
2019-12-14 06:12:30 -0600

    ver15
    db information
    ticker symbol char length
    python manage.py makemigrations &  python manage.py migrate

--- stocks/quotes/models.py
+++ stocks/quotes/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+#db information
 
 # Create your models here.
+class Stock(models.Model):
+    # ticker symbol char length
+    ticker = models.CharField(max_length=10)
+
+    # admin info for ticker registri.
+    def __str__(self):
+        return self.ticker
+
+    #once done with db info, python manage.py makemigrations &  python manage.py migrate
